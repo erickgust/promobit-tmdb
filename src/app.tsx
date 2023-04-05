1ae60db4e0db558844b9bae628c0a6ce15e84061
Erick Gustavo <erick.stazs@gmail.com>
2023-04-04 23:57:07 -0300

    feat(app): update poster and add movie title and release date

--- src/app.tsx
+++ src/app.tsx
@@ -2,16 +2,21 @@ import poster from '@/assets/poster.png'
 
 function App () {
   return (
-    <div className='inline-block'>
+    <div className='inline-block w-39 sm:w-44'>
       <img
         src={poster}
         alt='Poster'
-        className='object-cover rounded w-39 h-58 sm:h-66 sm:w-44'
+        className='object-cover rounded h-58 sm:h-66'
       />
 
       <div className='mt-2 flex flex-col'>
-        <strong>Oi, Alberto</strong>
-        <span>12 nov 2021</span>
+        <strong className='text-sm sm:text-base'>
+          Homem-Aranha: Sem Volta Para Casa
+        </strong>
+
+        <span className='text-xs sm:text-sm text-gray-500 uppercase'>
+          12 nov 2021
+        </span>
       </div>
     </div>
   )
