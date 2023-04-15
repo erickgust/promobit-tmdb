4f74b74acf13f0a1f83b679a823057083e9ec813
Erick Gustavo <erick.stazs@gmail.com>
2023-04-15 17:31:12 -0300

    chore: set cursor pointer

--- src/components/genre-item.tsx
+++ src/components/genre-item.tsx
@@ -6,7 +6,7 @@ export function GenreItem ({ genre }: GenreItemProps) {
   return (
     <li className='inline-block text-neutral-700'>
       <input type='checkbox' id={genre} className='peer hidden' />
-      <label htmlFor={genre} className='peer-checked:text-white py-1 px-4 bg-white rounded peer-checked:bg-yellow-600'>
+      <label htmlFor={genre} className='peer-checked:text-white py-2 cursor-pointer px-4 bg-white rounded peer-checked:bg-yellow-600'>
         {genre}
       </label>
     </li>
