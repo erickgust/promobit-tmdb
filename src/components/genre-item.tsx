1c4a0ed5cf329bb40ae33a23ca27de20d2c67d80
Erick Gustavo <erick.stazs@gmail.com>
2023-05-09 20:21:26 -0300

    feat: add dynamic genre selection and fetch genres from API

--- src/components/genre-item.tsx
+++ src/components/genre-item.tsx
@@ -1,13 +1,21 @@
 type GenreItemProps = {
-  genre: string
+  genreName: string
+  isSelected: boolean
+  onSelectedGenre: () => void
 }
 
-export function GenreItem ({ genre }: GenreItemProps) {
+export function GenreItem ({ genreName, isSelected, onSelectedGenre }: GenreItemProps) {
   return (
     <li>
-      <input type='checkbox' id={genre} className='peer hidden' />
-      <label htmlFor={genre} className='inline-block text-neutral-700 peer-checked:text-white py-2 cursor-pointer px-4 bg-white rounded peer-checked:bg-yellow-600'>
-        {genre}
+      <input
+        type='checkbox'
+        id={genreName}
+        className='peer hidden'
+        checked={isSelected}
+        onChange={onSelectedGenre}
+      />
+      <label htmlFor={genreName} className='inline-block text-neutral-700 peer-checked:text-white py-2 cursor-pointer px-4 bg-white rounded peer-checked:bg-yellow-600'>
+        {genreName}
       </label>
     </li>
   )
--- src/components/genres.tsx
+++ src/components/genres.tsx
@@ -1,8 +1,42 @@
+import { useEffect, useState } from 'react'
 import { GenreItem } from './genre-item'
 
-const genres = ['Ação', 'Aventura', 'Animação', 'Comédia', 'Crime', 'Documentário', 'Drama', 'Família', 'Fantasia', 'História', 'Terror', 'Música', 'Mistério', 'Romance', 'Ficção científica', 'Cinema TV', 'Thriller', 'Guerra', 'Faroeste']
+export type Genre = {
+  name: string
+  id: number
+}
+
+const apiKey = import.meta.env.VITE_TMDB_API_KEY
 
 export function Genres () {
+  const [genres, setGenres] = useState<Genre[]>([])
+  const [selectedGenres, setSelectedGenres] = useState<number[]>([])
+
+  useEffect(() => {
+    async function getGenres () {
+      const url = 'https://api.themoviedb.org/3/genre/movie/list?language=pt'
+
+      const response = await fetch(`${url}&api_key=${apiKey}`)
+      const data = await response.json()
+
+      setGenres(data.genres)
+    }
+
+    getGenres()
+  }, [])
+
+  function handleSelectedGenre (id: number) {
+    const alreadySelected = selectedGenres.includes(id)
+
+    if (alreadySelected) {
+      const filteredGenres = selectedGenres.filter(item => item !== id)
+
+      setSelectedGenres(filteredGenres)
+    } else {
+      setSelectedGenres(prevState => [...prevState, id])
+    }
+  }
+
   return (
     <header className='bg-[#2e1065] text-white sm:text-center px-4 py-10'>
       <div className='max-w-6xl mx-auto'>
@@ -17,7 +51,12 @@ export function Genres () {
 
           <ul className='flex gap-3 flex-wrap sm:justify-center'>
             {genres.map(genre => (
-              <GenreItem key={genre} genre={genre} />
+              <GenreItem
+                key={genre.id}
+                genreName={genre.name}
+                isSelected={selectedGenres.includes(genre.id)}
+                onSelectedGenre={() => handleSelectedGenre(genre.id)}
+              />
             ))}
           </ul>
         </nav>
