037b061f7328db02c61ac0d4e00658099ffa469b
Erick Gustavo <erick.stazs@gmail.com>
2023-05-18 01:53:45 -0300

    feat: add link to home in header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,15 @@
 import logo from '@/assets/icons/tmdb.svg'
+import { Link } from 'react-router-dom'
 
 export function Header () {
   return (
     <header>
-      <div className='
-        flex justify-center items-center bg-indigo-500 py-4
-        sm:pl-28 sm:justify-start
-      '>
+      <Link
+        to='/'
+        className='flex justify-center items-center bg-indigo-500 py-4 sm:pl-28 sm:justify-start'
+      >
         <img src={logo} alt='Logo' />
-      </div>
+      </Link>
     </header>
   )
 }
