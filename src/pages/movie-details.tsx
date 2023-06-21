d8724511094677d2bc5dde3f688203255028cd17
Erick Gustavo <erick.stazs@gmail.com>
2023-06-21 01:32:21 -0300

    feat(movie-details): add useQuery hook and refactor data fetching

--- src/pages/movie-details.tsx
+++ src/pages/movie-details.tsx
@@ -1,6 +1,6 @@
 import { z } from 'zod'
 import { useParams } from 'react-router-dom'
-import { useEffect, useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
 import { MovieCard } from '@/components/movie-card'
 import { CircularProgress } from '@/components/circular-progress'
 
@@ -125,13 +125,9 @@ function getReleaseInfo (movie: MovieReq) {
 
 export function MovieDetails () {
   const { movieId } = useParams()
-  const [movieData, setMovieData] = useState<Movie | null>(null)
-
-  const topCast = movieData?.credits.cast.slice(0, 16)
-  const topCrewMembers = movieData?.credits.crew.slice(0, 5)
-
-  useEffect(() => {
-    async function getDetails () {
+  const { data: movieData, isLoading, isError } = useQuery({
+    queryKey: ['movie', movieId],
+    queryFn: async () => {
       const movie = await moviesService.getMovieById(movieId)
 
       const releaseInfo = getReleaseInfo(movie)
@@ -143,7 +139,7 @@ export function MovieDetails () {
         Math.floor(movie.runtime / 60) + 'h ' + movie.runtime % 60 + 'm'
       )
 
-      setMovieData({
+      return {
         id: movie.id,
         genres: movie.genres,
         title: movie.title,
@@ -157,46 +153,63 @@ export function MovieDetails () {
         releaseDate,
         duration,
         userRating: Math.round(movie.vote_average * 10),
-      })
-    }
+      } as Movie
+    },
+  })
 
-    getDetails()
-  }, [movieId])
+  if (isLoading) {
+    return (
+      <div className='flex justify-center items-center h-screen'>
+        <p>Carregando...</p>
+      </div>
+    )
+  }
+
+  if (isError) {
+    return (
+      <div className='flex justify-center items-center h-screen'>
+        <p>Ocorreu um erro</p>
+      </div>
+    )
+  }
+
+  const topCast = movieData.credits.cast.slice(0, 16)
+  const topCrewMembers = movieData.credits.crew.slice(0, 5)
 
   return (
     <main>
       <header className='bg-[#2e1065] text-white p-4 py-16 sm:max-h-[36rem]'>
         <div className='w-full max-w-7xl mx-auto sm:flex items-start gap-8'>
           <img
-            src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2${movieData?.posterPath}`}
+            src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2${movieData.posterPath}`}
             alt='Poster'
             className='object-cover object-center rounded-lg w-44 sm:w-96 max-w-sm drop-shadow-md mx-auto my-9 sm:my-0'
           />
 
           <div className='w-full'>
             <h1 className='text-3xl font-bold mb-2'>
-              {movieData?.title}
+              {movieData.title}
             </h1>
 
             <div>
               <ul className='flex text-base font-normal flex-col sm:flex-row gap-2 sm:gap-0 flex-wrap'>
                 <li className='border p-1 h-min w-min rounded text-gray-400 border-gray-400 leading-none flex items-center'>
-                  {movieData?.ageRestriction || 'G'}
+                  {movieData.ageRestriction || 'G'}
                 </li>
 
                 <ListItem>
-                  {movieData?.releaseDate} ({movieData?.country})
+                  {movieData.releaseDate} ({movieData.country})
                 </ListItem>
 
                 <ListItem>
-                  {movieData?.genres.map(genre => genre.name).join(', ')}
+                  {movieData.genres.map(genre => genre.name).join(', ')}
                 </ListItem>
 
-                <ListItem>{movieData?.duration}</ListItem>
+                <ListItem>{movieData.duration}</ListItem>
               </ul>
 
               <div className='flex items-center justify-start gap-1 w-60 my-8'>
-                <CircularProgress size={75} percent={movieData?.userRating ?? 0} />
+                <CircularProgress size={75} percent={movieData.userRating ?? 0} />
                 <span className='inline-block text-base'>
                   Avaliação dos usuários
                 </span>
@@ -208,7 +221,7 @@ export function MovieDetails () {
                 </h2>
 
                 <p className='text-base'>
-                  {movieData?.overview}
+                  {movieData.overview}
                 </p>
               </div>
 
@@ -242,13 +255,13 @@ export function MovieDetails () {
           </div>
         </section>
 
-        {!!movieData?.trailer && (
+        {!!movieData.trailer && (
         <section className='mt-12 sm:mt-16'>
           <h2 className='font-bold text-2xl mb-4 sm:mb-6'>Trailer</h2>
 
           <div className='aspect-video mt-4 max-w-5xl'>
             <iframe
-              src={`https://www.youtube.com/embed/${movieData?.trailer.key}`}
+              src={`https://www.youtube.com/embed/${movieData.trailer.key}`}
               title='YouTube video player'
               allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
               className='w-full h-full'
@@ -261,7 +274,7 @@ export function MovieDetails () {
           <h2 className='font-bold text-2xl mb-4 sm:mb-6'>Recomendações</h2>
 
           <div className='flex gap-4 sm:gap-8 flex-wrap'>
-            {movieData?.recommendations.map(recommendation => (
+            {movieData.recommendations.map(recommendation => (
               <MovieCard
                 key={recommendation.id}
                 date={recommendation.release_date}
