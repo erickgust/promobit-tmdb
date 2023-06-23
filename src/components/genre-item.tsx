45c61d0024c379d7b9674b528407bdd302639f5e
Erick Gustavo <erick.stazs@gmail.com>
2023-06-23 15:10:44 -0300

    feat(genre-item): add close icon to selected genre

--- src/components/genre-item.tsx
+++ src/components/genre-item.tsx
@@ -1,3 +1,5 @@
+import { ReactComponent as CloseIcon } from '@/assets/icons/close-icon.svg'
+
 type GenreItemProps = {
   genreName: string
   isSelected: boolean
@@ -14,8 +16,14 @@ export function GenreItem ({ genreName, isSelected, onSelectedGenre }: GenreItem
         checked={isSelected}
         onChange={onSelectedGenre}
       />
-      <label htmlFor={genreName} className='inline-block text-neutral-700 peer-checked:text-white py-2 cursor-pointer px-4 bg-white rounded peer-checked:bg-yellow-600'>
+      <label htmlFor={genreName} className='inline-flex items-center justify-center text-neutral-700 peer-checked:text-white py-2 cursor-pointer px-4 bg-white rounded peer-checked:bg-yellow-600'>
         {genreName}
+        {isSelected && (
+          <CloseIcon
+            className='w-5 h-5 inline-block ml-2'
+            aria-label='Remove genre'
+          />
+        )}
       </label>
     </li>
   )
